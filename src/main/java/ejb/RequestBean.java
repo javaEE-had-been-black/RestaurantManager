3557b8242e1baca90227720d61b116fba2133f71
zcy <1229059439@qq.com>
2020-09-07 09:52:22 +0800

    ejb

--- src/main/java/ejb/RequestBean.java
+++ src/main/java/ejb/RequestBean.java
@@ -1,7 +1,18 @@
 package ejb;
 
+import javax.ejb.Stateful;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 /**
  * @author zhao chenyang
  */
+@Stateful
 public class RequestBean {
+    @PersistenceContext
+    private EntityManager em;
+    public void createUser(){
+
+    };
+
 }
