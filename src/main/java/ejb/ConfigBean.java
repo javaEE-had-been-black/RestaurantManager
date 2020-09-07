04a9187e8a616834eca39de6b2d8ff902997ae4f
zcy <1229059439@qq.com>
2020-09-07 09:29:46 +0800

    ejb

---
+++ src/main/java/ejb/ConfigBean.java
@@ -0,0 +1,27 @@
+package ejb;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.EJB;
+import javax.ejb.Startup;
+import javax.inject.Singleton;
+
+/**
+ * @author zhao chenyang
+ */
+@Singleton
+@Startup
+public class ConfigBean {
+    @EJB
+    private RequestBean request;
+
+    @PostConstruct
+    public void createData(){
+
+    }
+
+    @PreDestroy
+    public void deleteData(){
+
+    }
+}
