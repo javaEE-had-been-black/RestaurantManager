026272e9e670fafaeea8d9e5e08d187a5e155e26
suchen <962279610@qq.com>
2020-09-08 11:52:44 +0800

    添加了addUse、getAllItems方法，调整了对应注释格式

--- src/main/java/web/RestaurantManager.java
+++ src/main/java/web/RestaurantManager.java
@@ -1,11 +1,14 @@
 package web;
 
 import ejb.RequestBean;
+import entity.Repository;
 
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * @author zhang wen tao
@@ -15,26 +18,49 @@ import java.io.Serializable;
 public class RestaurantManager implements Serializable {
     @EJB
     private RequestBean request;
-    private Integer curr;
+    private static final Logger logger = Logger.getLogger("web.RestaurantManager");
     /**
-     *  Seat
+     * Seat
      */
     private Integer currentOrder;
     private String currentDish;
-    /**
-     *  User
+
+    /*
+      User
      */
-    //添加user
 
-    //删除user
+    /**
+     * 添加user
+     */
+    public void addUser(String userId,
+                        String userName,
+                        String password,
+                        String position,
+                        String telNumber,
+                        String salary) {
+        request.createUser(userId, userName, password, position, telNumber, salary);
+    }
 
+    /**
+     * 删除user
+     */
+    //TODO 等待张航编写新接口
+    public void removeUser() {
+
+    }
     //获取密码(拥有账号)
 
-    /**
+    /*
      * Repository
      */
-    //获取仓库所有item实体
 
+    /**
+     * 获取仓库所有item实体
+     * @return 仓库中所有实体的List
+     */
+    public List<Repository> getAllItems(){
+        return request.getAllItems();
+    }
     /**
      * Order
      */
