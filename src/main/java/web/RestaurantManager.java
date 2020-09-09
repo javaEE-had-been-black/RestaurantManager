ee089001c3634097b5b603e28bfd6f334231e1f8
suchen <962279610@qq.com>
2020-09-09 09:48:35 +0800

    添加getter setter

--- src/main/java/web/RestaurantManager.java
+++ src/main/java/web/RestaurantManager.java
@@ -178,6 +178,22 @@ public class RestaurantManager implements Serializable {
         }
     }
 
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     /**
      * Repository
      */
